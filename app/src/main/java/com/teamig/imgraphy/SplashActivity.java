b9034220fbdf5c127d356ea938e1b646f6d490e4
Novang <qkdxorjs1002@naver.com>
2020-11-04 03:54:06 +0900

    Add SplashActivity

---
+++ app/src/main/java/com/teamig/imgraphy/SplashActivity.java
@@ -0,0 +1,12 @@
+package com.teamig.imgraphy;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+}
