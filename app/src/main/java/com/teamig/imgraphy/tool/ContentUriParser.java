937ada03deaebc321fdba35397e8850668bc2264
Novang <qkdxorjs1002@naver.com>
2020-11-07 05:39:54 +0900

    Add content uri parser (temp)

---
+++ app/src/main/java/com/teamig/imgraphy/tool/ContentUriParser.java
@@ -0,0 +1,41 @@
+package com.teamig.imgraphy.tool;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+
+import java.io.FileDescriptor;
+
+public class ContentUriParser {
+
+    public static Uri getExternalPath(ContentResolver contentResolver, Uri contentUri) {
+        Uri imageUri;
+
+        if (contentUri.getPath().startsWith("/storage")) {
+            return contentUri;
+        }
+
+        Cursor cursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                new String[] { MediaStore.Images.ImageColumns._ID },
+                MediaStore.Images.ImageColumns._ID + "=?",
+                new String[] { contentUri.getPath().split(":")[1] },
+                MediaStore.Images.ImageColumns.DATE_ADDED + " DESC"
+        );
+
+        if (cursor.moveToFirst()) {
+            int idColNum = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns._ID);
+            long id = cursor.getLong(idColNum);
+            imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+            return imageUri;
+        }
+        cursor.close();
+
+        return null;
+    }
+}
