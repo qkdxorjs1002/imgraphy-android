bbdb24a1ebe5bb23232b6d792aef20a061ebb0ed
Novang <qkdxorjs1002@naver.com>
2020-11-10 04:21:56 +0900

    Rework splash

--- app/src/main/java/com/teamig/imgraphy/SplashActivity.java
+++ app/src/main/java/com/teamig/imgraphy/SplashActivity.java
@@ -3,12 +3,13 @@ package com.teamig.imgraphy;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.teamig.imgraphy.service.Imgraphy;
@@ -17,46 +18,50 @@ public class SplashActivity extends AppCompatActivity {
 
     private Imgraphy imgraphy;
 
+    private Button splashInitButton;
+    private Button splashAcceptButton;
+    private ConstraintLayout splashPolicyContainer;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        initReferences();
+        initEvents();
+    }
+
+    private void initReferences() {
+        splashInitButton = (Button) findViewById(R.id.SplashInitButton);
+        splashAcceptButton = (Button) findViewById(R.id.SplashAcceptButton);
+        splashPolicyContainer = (ConstraintLayout) findViewById(R.id.SplashPolicyContainer) ;
 
         SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE);
         final String userID = sharedPreferences.getString("userID", null);
 
-        if (userID == null) {
-            setContentView(R.layout.activity_splash);
-            onInit();
-        } else {
+        if (userID != null) {
             exitSplash();
         }
     }
 
-    private void onInit() {
-        imgraphy = new Imgraphy();
+    private void initEvents() {
+        splashInitButton.setOnClickListener(v -> {
+            splashPolicyContainer.setVisibility(View.VISIBLE);
+        });
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-        builder.setTitle("서비스 이용 약관");
-        builder.setMessage("<서비스 이용 약관에 대한 내용>");
-
-        builder.setPositiveButton("동의", (dialog, which) -> {
+        splashAcceptButton.setOnClickListener(v -> {
             Toast.makeText(this,"서비스 이용 약관에 동의하셨습니다.", Toast.LENGTH_SHORT).show();
+            imgraphy = new Imgraphy();
+
             imgraphy.generateID(true).observe(this, result -> {
                 SharedPreferences.Editor editor = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE).edit();
+
                 editor.putString("userID", result.log);
                 editor.apply();
 
                 exitSplash();
             });
         });
-
-        builder.setNegativeButton("거절", (dialog, which) -> {
-            Toast.makeText(this,"서비스 이용 약관에 거절하셨습니다.", Toast.LENGTH_SHORT).show();
-            closeApp();
-        });
-
-        builder.show();
     }
 
     private void exitSplash() {
@@ -64,11 +69,5 @@ public class SplashActivity extends AppCompatActivity {
         startActivity(intent);
 
         finish();
-    }
-
-    private void closeApp() {
-        moveTaskToBack(true);
-        finishAndRemoveTask();
-        System.exit(0);
     }
 }
